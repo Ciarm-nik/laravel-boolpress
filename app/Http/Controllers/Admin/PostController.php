7e7a503e52f89bcbd4a02882517374cc5396c85c
Ciarm-nik <nikcia@libero.it>
2021-07-02 17:22:37 +0200

    Creata relazione posts-User

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use  App\Post;
+use App\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
 
 class PostController extends Controller
 {

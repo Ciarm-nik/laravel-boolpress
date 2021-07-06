e26d76d2a7bf8e632c599d622da7934a8a813944
Ciarm-nik <nikcia@libero.it>
2021-07-06 15:40:48 +0200

    Creata rotta api e controllata tramite postman

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -15,11 +15,16 @@
                     <dd>{{ $post->title }}</dd>
                     <dt>Tags</dt>
                     <dd>
+                    @if(count($post->tags)>0)
                     @foreach($post->tags as $tag)
                     <span class="badge badge-primary">{{$tag->name}}</span>
                         
                     @endforeach
+                    @else
+                    <em>Nessun tag disponibile....</em>
+                    @endif
                     </dd>
+
                     {{-- <dt>Slug</dt>
                     <dd>{{ $post->slug }}</dd> --}}
                     <dt>Contenuto</dt>
